81a8c585cd2e45b02d2c5a5b45305d5af6adaeeb
Melvin Tong <tongman1516@gmail.com>
2024-11-11 14:34:21 +0800

    train12(11) & val11(10)

--- data/ImageNet1k/image_feature.py
+++ data/ImageNet1k/image_feature.py
@@ -74,9 +74,9 @@ with torch.inference_mode():
             # outputs = outputs.mean(dim=1).mean(dim=1)
             outputs = infer(processor, model, data)
             tensor = torch.cat((tensor, outputs), 0)
-            if not i % 1000:
-                torch.save(tensor, f"image_features/{args.split}/{args.split}_features_{i//1000}.pt")
-                print(f"{args.split} checkpoint {i//1000} saved")
+            if not i % 999 and i != 0:
+                torch.save(tensor, f"image_features/{args.split}/{args.split}_features_{(i+1)//1000}.pt")
+                print(f"{args.split} checkpoint {(i+1)//1000} saved")
                 tensor = torch.tensor([]).to(model.device)
         except Exception as e:
             with open(f"image_features/{args.split}_features_error.txt", "a") as f:
